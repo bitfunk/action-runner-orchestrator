03406321301436ecb4a1089b17a1fdb87b91955b
Wolf-Martell Montwé <wolf@bitfunk.eu>
2023-09-08 17:27:46 +0000

    Update github-action-orchestrator.main.kts
    Bump ktor 2.3.0 -> 2.3.4

--- src/main/kotlin/github-action-orchestrator.main.kts
+++ src/main/kotlin/github-action-orchestrator.main.kts
@@ -5,11 +5,11 @@
 @file:DependsOn("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.21")
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 @file:DependsOn("eu.jrie.jetbrains:kotlin-shell-core:0.2.1")
-@file:DependsOn("io.ktor:ktor-client-core-jvm:2.3.0")
-@file:DependsOn("io.ktor:ktor-client-cio-jvm:2.3.0")
-@file:DependsOn("io.ktor:ktor-client-logging-jvm:2.3.0")
-@file:DependsOn("io.ktor:ktor-client-content-negotiation-jvm:2.3.0")
-@file:DependsOn("io.ktor:ktor-serialization-gson-jvm:2.3.0")
+@file:DependsOn("io.ktor:ktor-client-core-jvm:2.3.4")
+@file:DependsOn("io.ktor:ktor-client-cio-jvm:2.3.4")
+@file:DependsOn("io.ktor:ktor-client-logging-jvm:2.3.4")
+@file:DependsOn("io.ktor:ktor-client-content-negotiation-jvm:2.3.4")
+@file:DependsOn("io.ktor:ktor-serialization-gson-jvm:2.3.4")
 @file:OptIn(ExperimentalCoroutinesApi::class)
 
 import com.google.gson.*
